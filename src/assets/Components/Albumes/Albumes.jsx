11c545123d798f58fab58f93496770bf9df613a7
Jerh011 <jeelrohe@gmail.com>
2025-07-10 00:31:25 -0700

    pantallas de artistas y albumes

--- src/assets/Components/Albumes/Albumes.jsx
+++ src/assets/Components/Albumes/Albumes.jsx
@@ -1,10 +1,37 @@
+import URL from "../../Services/URL";
+import "./Albumes.css";
 function Albumes() {
   return (
-    <>
-      <div>
-        <p>Albumes</p>
+    <div className="album-page ">
+      <div className="album-header">
+        <img
+          className="album-cover"
+          src={`${URL()}/media/abbey-road.jpg`}
+          alt="Portada del álbum Abbey Road"
+        />
+        <div className="album-descripcion">
+          <span className="album-type">Álbum</span>
+          <h1 className="album-title">Abbey Road</h1>
+          <p className="album-descripcion">The Beatles • 1969</p>
+        </div>
       </div>
-    </>
+
+      <div className="song-list-header">
+        <div>#</div>
+        <div>Título</div>
+        <div>Álbum</div>
+        <div>Fecha</div>
+        <div>Duración</div>
+      </div>
+
+      <div className="song-row">
+        <div>1</div>
+        <div>Come Together</div>
+        <div>Abbey Road</div>
+        <div>10 jul 2025</div>
+        <div>4:20</div>
+      </div>
+    </div>
   );
 }
 
--- src/assets/Components/Artistas/Artistas.jsx
+++ src/assets/Components/Artistas/Artistas.jsx
@@ -1,8 +1,117 @@
+import "./Artistas.css";
 function Artistas() {
+  const artistasEjemplo = [
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+    {
+      id: 1,
+      nombre: "Pink Floyd",
+      imagen: "https://api-musica.netlify.app/media/pink-floyd.jpeg",
+      info: "Reino Unido 1965",
+      descripcion:
+        "Banda británica de rock progresivo, conocida por sus álbumes conceptuales y sus innovaciones en el estudio.",
+    },
+  ];
   return (
-    <>
-      <p>Artistas</p>
-    </>
+    <div className="artistas-container">
+      <div className="lista-artistas">
+        {artistasEjemplo.map(({ id, nombre, imagen, info }) => (
+          <div key={id} className="artista-card">
+            <img src={imagen} alt={nombre} className="artista-imagen" />
+            <div className="artista-info">
+              <div className="artista-nombre">{nombre}</div>
+              <div className="artista-detalle">{info}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 export default Artistas;
